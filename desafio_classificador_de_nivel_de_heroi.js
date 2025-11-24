d5f24145d5fd7e5a61c07f194f17a7151be96339
João Victor Macêdo <joaovictornqm@gmail.com>
2025-11-24 17:18:09 -0300

    Enviando desafio DIO

---
+++ desafio_classificador_de_nivel_de_heroi.js
@@ -0,0 +1,27 @@
+//Realizando o desafio: Classificador de Nível de Herói
+let nomeHeroi = "João Victor"
+let xpHeroi = 8000
+var nivelHeroi
+
+if (xpHeroi <= 1000) {
+    nivelHeroi = "Ferro"
+} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze"
+} else if (xpHeroi > 2000 && xpHeroi <= 5000) {
+    nivelHeroi = "Prata"
+} else if (xpHeroi > 5000 && xpHeroi <= 7000) {
+    nivelHeroi = "Ouro"
+} else if (xpHeroi > 7000 && xpHeroi <= 8000) {
+    nivelHeroi = "Platina"
+} else if (xpHeroi > 8000 && xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente"
+} else if (xpHeroi > 9000 && xpHeroi <= 10000) {
+    nivelHeroi = "Imortal"
+} else {
+    nivelHeroi = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi)
+
+//Fim do desafio
+//Obrigado pela atenção.
